a0d635e2e13bee453782645095b7de62fa36388a
AkashDoke <akash.doke@arieotech.com>
2025-02-04 22:54:21 +0530

    !fix : src added

--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -9,7 +9,7 @@ print(os.getcwd())
 # Add the src directory to the Python path
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Add the src/ directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append("src")
 from meeting_recordings_analysis.main import meeting_minutes_flow
 
 # Initialize session state for navigation
