0823504e2339ac14852085e8c285cfc475d79426
AkashDoke <akash.doke@arieotech.com>
2025-02-04 22:47:13 +0530

    !fix : chck

--- src/meeting_recordings_analysis/streamlit_ui.py
+++ src/meeting_recordings_analysis/streamlit_ui.py
@@ -10,7 +10,7 @@ print(os.getcwd())
 #sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Add the src/ directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from meeting_recordings_analysis.main import meeting_minutes_flow
+from main import meeting_minutes_flow
 
 # Initialize session state for navigation
 if "page" not in st.session_state:
