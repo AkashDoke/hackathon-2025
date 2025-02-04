4793cb540dafd038c71675e17a6e96d41eeae4b1
Abhishek k <abhishek.kulkarni@arieotech.com>
2025-02-05 01:07:05 +0530

    kakak

--- src/streamlit_ui.py
+++ src/streamlit_ui.py
@@ -5,6 +5,11 @@ import urllib.request
 import zipfile
 import logging
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] - %(message)s")
 logger = logging.getLogger(__name__)
