03e5b3f1316d6c036abf07f73e22222ba38cb697
ankita chavan <ankita.chavan@lbmc.com>
2025-02-05 18:23:43 +0530

    UI changes

--- src/meeting_recordings_analysis/main.py
+++ src/meeting_recordings_analysis/main.py
@@ -1,3 +1,12 @@
+from crewai import Crew, LLM
+from dotenv import load_dotenv
+from meeting_recordings_analysis.jira.utils import parse_markdown, create_jira_issue, create_jira_task, get_active_sprint_id, add_to_sprint
+from pydub.utils import make_chunks
+from pydub import AudioSegment
+from meeting_recordings_analysis.tasks import Tasks
+from meeting_recordings_analysis.agents import Agents
+from pydantic import BaseModel
+from openai import OpenAI
 import os
 
 import agentops
@@ -6,21 +15,12 @@ import io
 
 os.environ["OTEL_SDK_DISABLED"] = "true"
 
-from openai import OpenAI
-from pydantic import BaseModel
-from meeting_recordings_analysis.agents import Agents
-from meeting_recordings_analysis.tasks import Tasks
-from crewai import Crew , LLM
-from pydub import AudioSegment
-from pydub.utils import make_chunks
 
-#import msal
-from meeting_recordings_analysis.jira.utils import parse_markdown, create_jira_issue, create_jira_task, get_active_sprint_id, add_to_sprint
-from dotenv import load_dotenv
+# import msal
 
 load_dotenv()
 
-#agentops.init(api_key=os.getenv("AGENT_OPS_KEY"), skip_auto_end_session=True)
+# agentops.init(api_key=os.getenv("AGENT_OPS_KEY"), skip_auto_end_session=True)
 
 # MSAL Authentication Configuration
 CLIENT_ID = os.getenv("CLIENT_ID")
@@ -29,14 +29,16 @@ AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 SCOPES = ["User.Read"]
 
 # State Management Class
+
+
 class MeetingMinutesState(BaseModel):
     transcript: str = ""
     meeting_minutes: str = ""
     meeting_minutes_faq: str = ""
     meeting_minutes_jira_tasks: str = ""
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=openai_api_key)
+# openai_api_key = os.getenv("OPENAI_API_KEY")
+# client = OpenAI(api_key=openai_api_key)
 
 
 class MeetingMinutesFlow:
@@ -62,16 +64,16 @@ class MeetingMinutesFlow:
         #     print(f"Transcribing chunk {i+1}/{len(chunks)}")
         #     chunk_path = f"chunk_{i}.wav"
         #     chunk.export(chunk_path, format="wav")
-            
+
         #     with open(chunk_path, "rb") as audio_file:
         #         transcription = client.audio.transcriptions.create(
-        #             model="whisper-1", 
+        #             model="whisper-1",
         #             file=audio_file
         #         )
         #         full_transcription += transcription.text + " "
 
         # print(full_transcription)
-        
+
         # Simulated full transcription
         full_transcription = """Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd quarter 2023 earnings call. I'm John Doe, CEO of FinTech Plus. We've had a stellar Q2 with a revenue of $125 million, a 25% increase year over year. Our gross profit margin stands at a solid 58%, due in part to cost efficiencies gained from our scalable business model. Our EBITDA has surged to $37.5 million, translating to a remarkable 30% EBITDA margin. Our net income for the quarter rose to $16 million, which is a noteworthy increase from $10 million in Q2 2022. Our total addressable market has grown substantially, thanks to the expansion of our high-yield savings product line and the new RoboAdvisor platform. We've been diversifying our asset-backed securities portfolio, investing heavily in collateralized debt obligations and residential mortgage-backed securities. We've also invested $25 million in AAA-rated corporate bonds, enhancing our risk-adjusted returns. As for our balance sheet, total assets reached $1.5 billion with total liabilities at $900 million, leaving us with a solid equity base of $600 million. Our debt to equity ratio stands at 1.5, a healthy figure considering our expansionary phase. We continue to see substantial organic user growth, with customer acquisition cost dropping by 15% and lifetime value growing by 25%. Our LTVCAC ratio is at an impressive 3.5x. In terms of risk management, we have a value-at-risk model in place with a 99% confidence level indicating that our maximum loss will not exceed 5 million in the next trading day. We've adopted a conservative approach to managing our leverage and have a healthy tier one capital ratio of 12.5%. Our forecast for the coming quarter is positive. We expect revenue to be around 135 million and 8% quarter over quarter growth driven primarily by our cutting edge blockchain solutions and AI driven predictive analytics. We're also excited about the upcoming IPO of our FinTech subsidiary Pay Plus, which we expect to raise 200 million. Significantly bolstering our liquidity and paving the way for aggressive growth strategies. We thank our shareholders for their continued faith in us and we look forward to an even more successful Q3. Thank you so much.
 Transcription: Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd quarter 2023 earnings call. I'm John Doe, CEO of FinTech Plus. We've had a stellar Q2 with a revenue of $125 million, a 25% increase year over year. Our gross profit margin stands at a solid 58%, due in part to cost efficiencies gained from our scalable business model. Our EBITDA has surged to $37.5 million, translating to a remarkable 30% EBITDA margin. Our net income for the quarter rose to $16 million, which is a noteworthy increase from $10 million in Q2 2022. Our total addressable market has grown substantially, thanks to the expansion of our high-yield savings product line and the new RoboAdvisor platform. We've been diversifying our asset-backed securities portfolio, investing heavily in collateralized debt obligations and residential mortgage-backed securities. We've also invested $25 million in AAA-rated corporate bonds, enhancing our risk-adjusted returns. As for our balance sheet, total assets reached $1.5 billion with total liabilities at $900 million, leaving us with a solid equity base of $600 million. Our debt to equity ratio stands at 1.5, a healthy figure considering our expansionary phase. We continue to see substantial organic user growth, with customer acquisition cost dropping by 15% and lifetime value growing by 25%. Our LTVCAC ratio is at an impressive 3.5x. In terms of risk management, we have a value-at-risk model in place with a 99% confidence level indicating that our maximum loss will not exceed 5 million in the next trading day. We've adopted a conservative approach to managing our leverage and have a healthy tier one capital ratio of 12.5%. Our forecast for the coming quarter is positive. We expect revenue to be around 135 million and 8% quarter over quarter growth driven primarily by our cutting edge blockchain solutions and AI driven predictive analytics. We're also excited about the upcoming IPO of our FinTech subsidiary Pay Plus, which we expect to raise 200 million. Significantly bolstering our liquidity and paving the way for aggressive growth strategies. We thank our shareholders for their continued faith in us and we look forward to an even more successful Q3. Thank you so much."""
@@ -82,10 +84,13 @@ Transcription: Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd 
         summarize_agent = self.agents.summarizer_agent()
         meeting_minutes_writer = self.agents.meeting_minutes_writer()
 
-        summarize_task = self.tasks.summarizer_agent_task(summarize_agent, self.state.transcript)
-        meeting_minutes_task = self.tasks.meeting_minutes_writing_task(meeting_minutes_writer)
+        summarize_task = self.tasks.summarizer_agent_task(
+            summarize_agent, self.state.transcript)
+        meeting_minutes_task = self.tasks.meeting_minutes_writing_task(
+            meeting_minutes_writer)
 
-        crew = Crew(agents=[summarize_agent, meeting_minutes_writer], tasks=[summarize_task, meeting_minutes_task])
+        crew = Crew(agents=[summarize_agent, meeting_minutes_writer], tasks=[
+                    summarize_task, meeting_minutes_task])
         result = crew.kickoff()
 
         gmail_agent = self.agents.gmail_draft_agent()
@@ -98,7 +103,8 @@ Transcription: Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd 
 
     def generate_meeting_minutes_faq(self):
         summarize_faq_agent = self.agents.summarizer_faq_agent()
-        summarize_faq_task = self.tasks.summarizer_faq_agent_task(summarize_faq_agent, self.state.transcript)
+        summarize_faq_task = self.tasks.summarizer_faq_agent_task(
+            summarize_faq_agent, self.state.transcript)
 
         crew = Crew(agents=[summarize_faq_agent], tasks=[summarize_faq_task])
         result = crew.kickoff()
@@ -157,11 +163,11 @@ Transcription: Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd 
                 trimmed_markdown = str(result).strip("```").strip()
                 story = parse_markdown(trimmed_markdown)
 
-                sprint_id = get_active_sprint_id()     
+                sprint_id = get_active_sprint_id()
 
                 if sprint_id:
                     print(sprint_id)
-                    story_id = create_jira_issue(story) 
+                    story_id = create_jira_issue(story)
                     print(story_id)
 
                     task_ids = []
@@ -174,12 +180,14 @@ Transcription: Good afternoon, everyone, and welcome to FinTech Plus Sync's 2nd 
                     print("successfully creted jira stories")
 
             else:
-                print("Kickoff did not complete successfully. Skipping subsequent steps.")
+                print(
+                    "Kickoff did not complete successfully. Skipping subsequent steps.")
 
             return result
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
+
 
 # Create an instance of the flow
 meeting_minutes_flow = MeetingMinutesFlow()
